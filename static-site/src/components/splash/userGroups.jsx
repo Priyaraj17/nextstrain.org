781039e3465c16056781f70a8b7a1e75ea3bda55
eharkins <eli.harkins@gmail.com>
2021-05-07 12:34:33 -0700

    /groups page intro text and list visibleGroups

--- static-site/src/components/splash/userGroups.jsx
+++ static-site/src/components/splash/userGroups.jsx
@@ -5,40 +5,41 @@ import Cards from "../Cards";
 import { HugeSpacer, FlexCenter } from "../../layouts/generalComponents";
 import { theme } from "../../layouts/theme";
 
+const colors = [...theme.titleColors];
+
+const groupCards = (groups) => groups.map((group) => {
+  const groupColor = colors[0];
+  colors.push(colors.shift());
+
+  return (
+    {
+      img: "empty.png",
+      url: `/groups/${group.name}`,
+      title: group.name,
+      color: groupColor,
+      private: group.private
+    }
+  );
+});
+
 const UserGroups = (props) => {
-  const colors = [...theme.titleColors];
-
-  const groupCards = props.visibleGroups.map((group) => {
-    const groupColor = colors[0];
-    colors.push(colors.shift());
-
-    return (
-      {
-        img: "empty.png",
-        url: `/groups/${group.name}`,
-        title: group.name,
-        color: groupColor,
-        private: group.private
-      }
-    );
-  });
-
+  const visibleGroups = props.visibleGroups || [];
   return (
     <Fragment>
       <ScrollableAnchor id={'groups'}>
         <Styles.H1>Nextstrain Groups</Styles.H1>
       </ScrollableAnchor>
 
-      <FlexCenter>
+      {props.user && <FlexCenter>
         <Styles.CenteredFocusParagraph>
           Nextstrain groups represent collections of datasets, potentially with controlled access.
           You (
           <Styles.StrongerText>{props.user.username}</Styles.StrongerText>
           ) have access to the following groups (a padlock icon indicates a private group):
         </Styles.CenteredFocusParagraph>
-      </FlexCenter>
+      </FlexCenter>}
 
-      <Cards cards={groupCards} squashed/>
+      <Cards cards={groupCards(visibleGroups)} squashed/>
 
       <HugeSpacer/>
     </Fragment>
